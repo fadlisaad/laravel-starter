d6552a8ddc9470efefb7ed974bd1f5225482f105
Fadli Saad <fadlisaad@gmail.com>
2021-05-26 14:48:53 +0800

    change logo
    Signed-off-by: Fadli Saad <fadlisaad@gmail.com>

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -1,5 +1,5 @@
 <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
-    <button class="c-header-toggler c-class-toggler d-lg-none mr-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button><a class="c-header-brand d-sm-none" href="{{route("backend.dashboard")}}"><img class="c-header-brand" src="{{asset("img/backend-logo.jpg")}}" style="max-height:50px;min-height:40px;" alt="{{ app_name() }}"></a>
+    <button class="c-header-toggler c-class-toggler d-lg-none mr-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button><a class="c-header-brand d-sm-none" href="{{route("backend.dashboard")}}"><img class="c-header-brand" src="{{asset("img/backend-logo.png")}}" style="max-height:50px;min-height:40px;" alt="{{ app_name() }}"></a>
     <button class="c-header-toggler c-class-toggler ml-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true"><span class="c-header-toggler-icon"></span></button>
 
     <ul class="c-header-nav d-md-down-none">
